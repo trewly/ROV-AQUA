6929cf0aa71b2defe41ce502fecea3d8b3599ab7
Trewly <nguyenductrung160602@gmail.com>
2025-03-07 11:39:23 +0700

    video-streaming2

--- Mission_planner/controller/video.py
+++ Mission_planner/controller/video.py
@@ -1,17 +1,11 @@
-from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5 import QtGui
-import cv2
+import subprocess
 
-class VideoThread(QThread):
-    change_pixmap_signal = pyqtSignal(QtGui.QImage)
+port = "5000"
 
-    def run(self):
-        cap = cv2.VideoCapture(0)
-        while True:
-            ret, frame = cap.read()
-            if ret:
-                rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                h, w, ch = rgb_image.shape
-                bytes_per_line = ch * w
-                qt_image = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-                self.change_pixmap_signal.emit(qt_image)
+gst_command = (
+    f"gst-launch-1.0 udpsrc port={port} ! application/x-rtp, encoding-name=H264 ! "
+    "rtph264depay ! avdec_h264 ! videoconvert ! d3dvideosink"
+)
+
+print(f"Receiving stream on PC from Raspberry Pi on port {port}...")
+subprocess.run(gst_command, shell=True)
