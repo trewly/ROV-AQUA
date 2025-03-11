c13e40404f12c1e9b4f987720ed61ceb99c0c217
khanhisme1 <khanhisme1@gmail.com>
2025-03-11 12:50:47 +0700

    function to receive file

---
+++ Mission_planner/communication/pc_receive.py
@@ -0,0 +1,52 @@
+import socket
+import subprocess
+
+def receive_image(server_ip, server_port, save_path):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen(1)
+    print("Đang chờ kết nối từ Raspberry Pi...")
+
+    conn, addr = server_socket.accept()
+    print(f"Kết nối từ: {addr}")
+
+    file_name = b""
+    while not file_name.endswith(b"\n"):
+        file_name += conn.recv(1)
+    file_name = file_name.decode().strip()
+    
+    save_path = file_name if save_path is None else save_path
+    with open(save_path, "wb") as file:
+        while chunk := conn.recv(4096):
+            file.write(chunk)
+
+    print(f"Đã nhận file và lưu tại: {save_path}")
+    conn.close()
+    server_socket.close()
+
+#receive_image(server_ip="0.0.0.0", server_port=5001, save_path="output.jpg")
+
+def receive_video(server_ip, server_port, save_path):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen(1)
+    print("Đang chờ kết nối từ Raspberry Pi...")
+
+    conn, addr = server_socket.accept()
+    print(f"Kết nối từ: {addr}")
+
+    with open(save_path, "wb") as file:
+        while chunk := conn.recv(4096):
+            file.write(chunk)
+
+    print(f"Đã nhận video và lưu tại: {save_path}")
+    conn.close()
+    server_socket.close()
+
+    mp4_path = save_path.replace(".h264", ".mp4")
+    cmd = f'ffmpeg -y -framerate 30 -i "{save_path}" -c copy "{mp4_path}"'
+    subprocess.run(cmd, shell=True, check=True)
+
+    print(f"Đã chuyển đổi thành: {mp4_path}")
+
+receive_video(server_ip="0.0.0.0", server_port=5001, save_path="received_video.h264")
